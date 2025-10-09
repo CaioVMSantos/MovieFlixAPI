8cf060ee0a17673af23d254ec0f83e58f41d7659
CaioVMSantos <caiovictorcvm10@gmail.com>
2025-10-09 18:41:05 -0300

    feat: Implements Movie logic, service, controller with create and get all methods, Dto and Mapper with db relationship

--- src/main/java/br/com/movieflix/controller/MovieController.java
+++ src/main/java/br/com/movieflix/controller/MovieController.java
@@ -1,9 +1,16 @@
 package br.com.movieflix.controller;
 
+import br.com.movieflix.entity.Movie;
+import br.com.movieflix.mapper.MovieMapper;
+import br.com.movieflix.request.MovieRequest;
+import br.com.movieflix.response.MovieResponse;
 import br.com.movieflix.service.MovieService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movieflix/movie")
@@ -11,5 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieController {
 
     private final MovieService movieService;
+
+    @GetMapping()
+    public ResponseEntity<List<MovieResponse>> findAll() {
+        return ResponseEntity.ok(movieService.findAll()
+                .stream()
+                .map(MovieMapper::toMovieResponse)
+                .toList());
+    }
+
+    @PostMapping
+    public ResponseEntity<MovieResponse> createMovie(@RequestBody MovieRequest request) {
+        Movie movieSaved = movieService.create(MovieMapper.toMovie(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(MovieMapper.toMovieResponse(movieSaved));
+    }
 
 }
--- src/main/java/br/com/movieflix/service/MovieService.java
+++ src/main/java/br/com/movieflix/service/MovieService.java
@@ -1,7 +1,10 @@
 package br.com.movieflix.service;
 
+import br.com.movieflix.entity.Movie;
 import br.com.movieflix.repository.MovieRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MovieService {
@@ -10,6 +13,14 @@ public class MovieService {
 
     public MovieService(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
+    }
+
+    public List<Movie> findAll() {
+        return movieRepository.findAll();
+    }
+
+    public Movie create(Movie movie) {
+        return movieRepository.save(movie);
     }
 
 }
