d3dd63dba73da8c7e77e61392da84c01e091a26c
CaioVMSantos <caiovictorcvm10@gmail.com>
2025-10-09 15:26:44 -0300

    feat: Implements Streaming endpoints and create controller, service, repository, dto, mapper

---
+++ src/main/java/br/com/movieflix/request/StreamingRequest.java
@@ -0,0 +1,4 @@
+package br.com.movieflix.request;
+
+public record StreamingRequest(String name) {
+}
---
+++ src/main/java/br/com/movieflix/service/StreamingService.java
@@ -0,0 +1,35 @@
+package br.com.movieflix.service;
+
+import br.com.movieflix.entity.Streaming;
+import br.com.movieflix.repository.StreamingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StreamingService {
+
+    private final StreamingRepository streamingRepository;
+
+    public StreamingService(StreamingRepository streamingRepository) {
+        this.streamingRepository = streamingRepository;
+    }
+
+    public List<Streaming> findAll() {
+        return streamingRepository.findAll();
+    }
+
+    public Streaming create(Streaming streaming){
+        return streamingRepository.save(streaming);
+    }
+
+    public Optional<Streaming> findById(Long id){
+        return streamingRepository.findById(id);
+    }
+
+    public void deleteById(Long id){
+        streamingRepository.deleteById(id);
+    }
+
+}
