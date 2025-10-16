27a92428280f2d2adb733a9260a2605e1de617db
CaioVMSantos <caiovictorcvm10@gmail.com>
2025-10-16 17:06:03 -0300

    feat: Implements SecurityFilter and JWTUserData for token validation

--- src/main/java/br/com/movieflix/config/TokenService.java
+++ src/main/java/br/com/movieflix/config/TokenService.java
@@ -3,11 +3,13 @@ package br.com.movieflix.config;
 import br.com.movieflix.entity.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
 import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class TokenService {
@@ -27,6 +29,24 @@ public class TokenService {
                 .withIssuedAt(Instant.now())
                 .withIssuer("API MovieFlix")
                 .sign(algorithm);
+    }
+
+    public Optional<JWTUserData> validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            DecodedJWT decode = JWT.require(algorithm)
+                    .build()
+                    .verify(token);
+
+            return Optional.of(JWTUserData.builder()
+                    .email(decode.getSubject())
+                    .userId(decode.getClaim("userId").asLong())
+                    .build());
+
+        } catch (JWTVerificationException ex) {
+            return Optional.empty();
+        }
     }
 
 }
